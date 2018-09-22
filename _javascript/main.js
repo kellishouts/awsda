9f091573f133e559e171303d51185d108c0ea64a
Marianne Feng <feng.marianne@gmail.com>
2018-09-22 11:35:57 -0700

    added bulma navbar

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
+  //this handles navbar on click
+  var burger = document.querySelector('.burger');
+  var nav = document.querySelector('#'+burger.dataset.target);
+
+  burger.addEventListener('click', function(){
+      burger.classList.toggle('is-active');
+      nav.classList.toggle('is-active');
+  });
 });
--- compartments/assets/lib/main.js
+++ compartments/assets/lib/main.js
@@ -1,5 +1,12 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('Hello Bulma!');
+  //this handles navbar on click
+  var burger = document.querySelector('.burger');
+  var nav = document.querySelector('#' + burger.dataset.target);
+
+  burger.addEventListener('click', function () {
+    burger.classList.toggle('is-active');
+    nav.classList.toggle('is-active');
+  });
 });
