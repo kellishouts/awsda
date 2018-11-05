6ba00804817d0a929dc52477da7aa8b40e0d1b53
Marianne Feng <feng.marianne@gmail.com>
2018-11-04 22:44:59 -0800

    added javascript for new visualizations

--- _javascript/main.js
+++ _javascript/main.js
@@ -22,12 +22,28 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    var scriptElement = document.createElement('script');
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+
     //configures tableau graphic
     var divElement = document.getElementById('viz1541398310162');
     var vizElement = divElement.getElementsByTagName('object')[0];
     vizElement.style.width = '100%';
     vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
-    var scriptElement = document.createElement('script');
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
     vizElement.parentNode.insertBefore(scriptElement, vizElement);
+
+
+    //configures assault by age graphic
+    var assaultDivElement = document.getElementById('viz1541398984149');
+    var vizAssaultElement = assaultDivElement.getElementsByTagName('object')[0];
+    vizAssaultElement.style.width = '100%';
+    vizAssaultElement.style.height = (assaultDivElement.offsetWidth * 0.75) + 'px';
+    vizAssaultElement.parentNode.insertBefore(scriptElement, vizAssaultElement);
+
+    //configures assault by county
+    var divCountyElement = document.getElementById('viz1541399127056');
+    var vizCountyElement = divCountyElement.getElementsByTagName('object')[0];
+    vizCountyElement.style.width = '100%';
+    vizCountyElement.style.height = (divCountyElement.offsetWidth * 0.75) + 'px';
+    vizCountyElement.parentNode.insertBefore(scriptElement, vizCountyElement);
 });
